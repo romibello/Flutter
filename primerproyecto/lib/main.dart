4645bc7b66979fb6a31d830f13e5dc6eff0fac99
Romina Bello <romina.bello@globant.com>
2020-01-27 17:46:05 -0300

    class reviewlist and review

--- primerproyecto/lib/main.dart
+++ primerproyecto/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'description_place.dart';
+import 'review_list.dart';
+
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   final dummyDescription = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." 
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book 3." ;
+  
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,7 +24,8 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.red[900],
         ),
-        body: new DescriptionPlace("bahamas", 4, dummyDescription) ,
+        body: new DescriptionPlace("bahamas", 4, dummyDescription) ,//new ReviewList()
+        
       ),//MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
